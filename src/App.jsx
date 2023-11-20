033a6b279750b00bd896c81399af576d1902d908
kiminagon <chto22089@g.nihon-u.ac.jp>
2023-11-20 14:45:49 +0900

    どうだ？

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,69 @@
+import React, { useEffect, useState } from 'react';
+
 export default function App() {
-    return (
-      <>
-        <header>
-          <h1>The Can Store</h1>
-        </header>
-        <div>
-          <aside>
-            <form>
-              <div>
-                <label htmlFor="category">Choose a category:</label>
-                <select id="category">
-                  <option>All</option>
-                  <option>Vegetables</option>
-                  <option>Meat</option>
-                  <option>Soup</option>
-                </select>
-              </div>
-              <div>
-                <label htmlFor="searchTerm">Enter search term:</label>
-                <input type="text" id="searchTerm" placeholder="e.g. beans" />
-              </div>
-              <div>
-                <button>Filter results</button>
-              </div>
-            </form>
-          </aside>
-          <main></main>
-        </div>
-        <footer>
-          <p>All icons found at the Noun Project:</p>
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    // Fetch APIを使用してJSONデータを取得する
+    fetch('products.json')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        // 取得したデータをReactのstateに設定する
+        setProducts(data);
+      })
+      .catch(error => {
+        console.error('Fetch error:', error);
+      });
+  }, []); // 空の依存配列を渡すことで、マウント時のみ実行される
+
+  return (
+    <>
+      <header>
+        <h1>The Can Store</h1>
+      </header>
+      <div>
+        <aside>
+        <form>
+            <div>
+              <label htmlFor="category">Choose a category:</label>
+              <select id="category">
+                <option>All</option>
+                <option>Vegetables</option>
+                <option>Meat</option>
+                <option>Soup</option>
+              </select>
+            </div>
+            <div>
+              <label htmlFor="searchTerm">Enter search term:</label>
+              <input type="text" id="searchTerm" placeholder="e.g. beans" />
+            </div>
+            <div>
+              <button>Filter results</button>
+            </div>
+          </form>
+        </aside>
+        <main>
+            <ul>
+            {products.map(product => (
+              <div key={product.name} className={product.type}>
+                <h2>{product.name}</h2>
+                <p>{product.price}</p>
+                <img
+                  src={`images/${product.image}`}
+                  alt={product.name}
+                />
+                </div>
+            ))}
+            </ul>
+        </main>
+      </div>
+      <footer>
+      <p>All icons found at the Noun Project:</p>
           <ul>
             <li>
               Bean can icon by{" "}
@@ -47,7 +82,12 @@ export default function App() {
               <a href="https://thenounproject.com/smashicons/">Oliviu Stoian</a>.
             </li>
           </ul>
-        </footer>
-      </>
-    );
-  }
+      </footer>
+    </>
+  );
+}
+
+
+
+
+
